6122ef227f6d5a1a4ad183f47ca555e5f3f225fc
amir <amir123fg5@gmail.com>
2022-12-23 16:36:10 +0330

    WIP: crete main.tsx in workflow component

---
+++ components/workflow/main.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface MainProps {
+    
+}
+ 
+const Main: FunctionComponent<MainProps> = () => {
+    return <div></div>;
+}
+ 
+export default Main;
