e900a4fd5106eff9c01f72f7464437e9023b0237
amir <amir123fg5@gmail.com>
2022-12-23 16:37:19 +0330

    WIP: create main component

---
+++ components/workflow/home/task/main.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface TaskProps {
+    
+}
+ 
+const Task: FunctionComponent<TaskProps> = () => {
+    return <div></div>;
+}
+ 
+export default Task;
