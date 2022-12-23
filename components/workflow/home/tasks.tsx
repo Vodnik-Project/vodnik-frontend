abc79445d0a55f483b4b9f16c36fb6fdee3d0ea9
amir <amir123fg5@gmail.com>
2022-12-23 16:36:55 +0330

    WIP: create three components

---
+++ components/workflow/home/tasks.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface TasksProps {
+    
+}
+ 
+const Tasks: FunctionComponent<TasksProps> = () => {
+    return <div></div>;
+}
+ 
+export default Tasks;
