33310a2ea0b96492c98ad49dc3f75bc58da30ca5
amir <amir123fg5@gmail.com>
2022-12-24 18:40:32 +0330

    WIP: render navbar directory from workflow directory

--- components/workflow/main.tsx
+++ components/workflow/main.tsx
@@ -1,11 +1,14 @@
 import { FunctionComponent } from "react";
 
+import Navbar from "./navbar/main";
 interface MainProps {
     
 }
+
  
 const Main: FunctionComponent<MainProps> = () => {
-    return <div></div>;
+    return <div>
+    </div>;
 }
  
 export default Main;
--- pages/workflow.tsx
+++ pages/workflow.tsx
@@ -1,12 +1,13 @@
 import { FunctionComponent } from "react";
 
+import Main from '../components/workflow'
 interface WorkFlowProps {
     
 }
  
 const WorkFlow: FunctionComponent<WorkFlowProps> = () => {
     return <div className=" ">
-        
+        <Main />
     </div>;
 }
  
