79f7ccc9a9227840c6d7430b94c77101b959822d
amir <amir123fg5@gmail.com>
2022-12-24 18:24:43 +0330

    ADD: crate navbar component in workflow directory

---
+++ components/workflow/navbar.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface NavbarProps {
+    
+}
+ 
+const Navbar: FunctionComponent<NavbarProps> = () => {
+    return <div>
+        
+    </div>;
+}
+ 
+export default Navbar;
