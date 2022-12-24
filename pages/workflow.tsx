77541ecc78e8ad83acd140c05c9ba73fdf22dfdc
amir <amir123fg5@gmail.com>
2022-12-24 17:36:42 +0330

    ADD: add a new component to Page directory as a route for workflow

---
+++ pages/workflow.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface WorkFlowProps {
+    
+}
+ 
+const WorkFlow: FunctionComponent<WorkFlowProps> = () => {
+    return <div className=" ">
+        
+    </div>;
+}
+ 
+export default WorkFlow;
