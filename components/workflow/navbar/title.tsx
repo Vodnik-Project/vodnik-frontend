a216429de46c9ac6bd9962c0fdb3854f48b998a7
amir <amir123fg5@gmail.com>
2022-12-26 19:40:26 +0330

    ADD: add title component to workflow component

---
+++ components/workflow/navbar/title.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface titleProps {
+    
+}
+ 
+const Title: FunctionComponent<titleProps> = () => {
+    return <div></div>;
+}
+ 
+export default Title;
